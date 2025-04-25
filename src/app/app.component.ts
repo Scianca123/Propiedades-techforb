1823084b49d056e6d3b9effda27ff899bb01bafd
Scianca123 <matias.sciancalepore@techforb.com>
2025-04-25 12:11:04 -0300

    add: seccion explore arrancada agregada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { MainSectionComponent } from './shared/components/main-section/main-section.component';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,MainSectionComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/components/main-section/main-section.component.ts
+++ src/app/shared/components/main-section/main-section.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { SelectorToggleComponent } from '../selector-toggle/selector-toggle.component';
 import { SearchFiltersPanelComponent } from '../search-filters-panel/search-filters-panel.component';
 
 @Component({
   selector: 'app-main-section',
-  imports: [SelectorToggleComponent,SearchFiltersPanelComponent],
+  imports: [SearchFiltersPanelComponent],
   templateUrl: './main-section.component.html',
   styleUrl: './main-section.component.scss'
 })
