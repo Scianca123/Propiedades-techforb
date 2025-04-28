4144a329fd8195ef2bc08d9f28a57feec5a3eb78
Scianca123 <matias.sciancalepore@techforb.com>
2025-04-28 15:02:03 -0300

    ADD: agrego la disposicion de estructura del dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './shared/pages/home/home.component';
 import { authGuardGuard } from './core/guards/auth-guard.guard';
 import { DashboardComponent } from './shared/pages/dashboard/dashboard.component';
+import { DashboardHomeComponentComponent } from './shared/pages/dashboard-home-component/dashboard-home-component.component';
 
 export const routes: Routes = [
       {
@@ -11,6 +12,11 @@ export const routes: Routes = [
       {
         path: 'dashboard',
         component: DashboardComponent,
-        canActivate: [authGuardGuard], // Aplica el guard a esta ruta
+        canActivate: [authGuardGuard], 
+        children: [
+          {
+            path: '',
+            component: DashboardHomeComponentComponent, // Página principal del dashboard
+          }]
       }
 ];
--- src/app/shared/pages/dashboard/dashboard.component.ts
+++ src/app/shared/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { DashboardHeaderComponent } from "../../components/dashboard-header/dashboard-header.component";
+import { MainContentComponent } from "../../components/main-content/main-content.component";
+import { DashboarFooterComponent } from "../../components/dashboar-footer/dashboar-footer.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [DashboardHeaderComponent, MainContentComponent, DashboarFooterComponent,RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
